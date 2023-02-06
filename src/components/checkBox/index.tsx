5d1ccd498614d2390e385ee4cb47436ccd2c82f9
Kimkyungmin123 <rud3302@naver.com>
2023-02-06 14:20:23 +0900

    chore: create check box component

---
+++ src/components/checkBox/index.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, ReactNode } from 'react';
+
+import { CheckboxContainer, CheckBoxLabel, CheckboxWrapper } from './style';
+
+interface CheckBoxProps {
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  isChecked: boolean;
+  children: ReactNode;
+  id: string;
+  name?: string;
+}
+
+const CheckBox = ({
+  onChange,
+  isChecked,
+  children,
+  id,
+  name,
+}: CheckBoxProps) => {
+  return (
+    <CheckboxContainer>
+      <CheckboxWrapper
+        type="checkbox"
+        checked={isChecked}
+        onChange={onChange}
+        id={id}
+        name={name}
+      />
+      <CheckBoxLabel htmlFor={id}>{children}</CheckBoxLabel>
+    </CheckboxContainer>
+  );
+};
+
+export default CheckBox;
