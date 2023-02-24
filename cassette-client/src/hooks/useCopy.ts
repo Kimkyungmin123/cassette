df88fd7737bd7c780c8a40d6f0d7553fcf4b5900
Kimkyungmin123 <rud3302@naver.com>
2023-02-24 09:44:20 +0000

    push to develop

--- cassette-client/src/hooks/useCopy.ts
+++ cassette-client/src/hooks/useCopy.ts
@@ -4,27 +4,38 @@ type onCopyType = (text: string) => Promise<boolean>;
 
 const useCopy = (): [boolean, onCopyType] => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
-  const onCopy: onCopyType = async (text: string) => {
-    try {
-      if (navigator.clipboard && navigator.clipboard.writeText) {
-        await navigator.clipboard.writeText(text);
-        setIsCopied(true);
-      } else {
-        const textarea = document.createElement('textarea');
-        textarea.value = text;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
-        setIsCopied(true);
-      }
 
+  const copyToClipboard = async (text: string) => {
+    try {
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+
+      setIsCopied(true);
       return true;
     } catch (error) {
       console.error(error);
       setIsCopied(false);
-
       return false;
+    }
+  };
+
+  const onCopy: onCopyType = async (text: string) => {
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      try {
+        await navigator.clipboard.writeText(text);
+        setIsCopied(true);
+        return true;
+      } catch (error) {
+        console.error(error);
+        setIsCopied(false);
+        return copyToClipboard(text);
+      }
+    } else {
+      return copyToClipboard(text);
     }
   };
 
