2b3464e48f0c2115eb00e4fa7e9a9d629296fd5d
Kimkyungmin123 <rud3302@naver.com>
2023-03-30 19:27:10 +0000

    push to main

--- cassette-client/src/pages/_app.tsx
+++ cassette-client/src/pages/_app.tsx
@@ -103,42 +103,6 @@ const App = ({ Component, pageProps }: AppProps) => {
           />
           <link rel="apple-touch-icon" href="favicons/apple-icon.png"></link>
           <meta name="msapplication-TileColor" content="#242729"></meta>
-          <link
-            href="splashscreens/iphone5_splash.png"
-            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphone6_splash.png"
-            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphoneplus_splash.png"
-            media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonex_splash.png"
-            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonexr_splash.png"
-            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonexsmax_splash.png"
-            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-
-          <link
-            href="splashscreens/ipadpro3_splash.png"
-            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
         </Head>
       </html>
       <Script
